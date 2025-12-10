ea5bdea0550f5ce19be32cb495604f183efe2fe3
agrim08 <agrimgupta0805@gmail.com>
2025-12-11 02:33:54 +0530

    Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+
+const Hero = () => {
+    const videoRef = useRef()
+
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playBackRate = 2;
+    })
+
+  return (
+    <section id='hero'>
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/title.png" alt="MacBook title" />
+        </div>
+
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
+
+        <button>Buy</button>
+
+        <p>From ₹169,900 or ₹8,325/mo for 12 months</p>
+    </section>
+  )
+}
+
+export default Hero
